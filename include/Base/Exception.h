6f444e2c39f573ac021761159d1b18d35a730d1c
daimingze <anyingshashou@qq.com>
2020-12-18 01:44:33 +0800

    config for macos compiler

--- include/Base/Exception.h
+++ include/Base/Exception.h
@@ -4,7 +4,7 @@
 #include <sstream>
 
 #define EXCEPT(...)  Exception(__LINE__,__FILE__,##__VA_ARGS__)
-#define THROW_EXCEPT(...) throw EXCEPT(##__VA_ARGS__)
+#define THROW_EXCEPT(...) throw EXCEPT(__VA_ARGS__)
 
 class Exception : public std::exception
 {
@@ -13,13 +13,13 @@ public:
         m_line(line), m_file(file)
     {}
     explicit Exception(int line, const char* file, char const* const message) noexcept :
-        m_line(line), m_file(file), std::exception(message)
+        m_line(line), m_file(file), m_message(message)
     {}
     const char* what() const noexcept override
     {
         std::ostringstream oss;
         oss << "[Type       ] " << GetType() << std::endl
-            << "[Description] " << std::exception::what() << std::endl
+            << "[Description] " << m_message << std::endl
             << GetOriginString() << std::endl;
         m_whatBuffer = oss.str();
         return m_whatBuffer.c_str();
@@ -40,6 +40,7 @@ public:
 private:
     int m_line;
     std::string m_file;
+    std::string m_message;
 protected:
     mutable std::string m_whatBuffer;;
 };
