4a8d0897ce01a04aceb79662d0bf78eb783be170
daimingze <anyingshashou@qq.com>
2020-12-19 02:37:52 +0800

    set cmake command args macOS-arm64

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 cd `dirname $0`
-cmake -S . -B out/build
+# build in unix like system?
+# cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B out/build
+# build macOS-Universal
+# cmake -DCMAKE_OSX_ARCHITECTURES=arm64;x86_64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B out/build
+# build macOS-arm64
+cmake -DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B out/build
+# build macOS-x86_64
+# cmake -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B out/build
 cd out/build
 make
