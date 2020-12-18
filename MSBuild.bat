64dab719a9d51a4dd2390138fc83490cfac040d4
HuiSeLiMing <1055907692@qq.com>
2020-12-18 21:43:24 +0800

    MSBuild command

--- MSBuild.bat
+++ MSBuild.bat
@@ -4,4 +4,6 @@ echo off
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 rem Generate Vsual Studio 2019 project.
 cmake -G "Visual Studio 16 2019" -A "x64" -S . -B out/build
+cd out/build
+MSBuild.exe ALL_BUILD.vcxproj -property:Configuration=Release
 pause
