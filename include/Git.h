d62abd7fcc8c88620e62d714ae780f255e632a4d
HuiSeLiMing <1055907692@qq.com>
2020-12-23 10:55:59 +0800

    fix gitignore git.h

--- include/Git.h
+++ include/Git.h
@@ -5,7 +5,7 @@
 #define GIT_RETRIEVED_STATE false
 
 // The SHA1 for the HEAD of the repo.
-#define GIT_HEAD_SHA1 L"4f43eb18edb7bac15658634e21088a9abecdf4f0"
+#define GIT_HEAD_SHA1 L"cf1abd68c8584702e564d088dff25ca83598e2c7"
 
 // Whether or not there were uncommited changes present.
 #define GIT_IS_DIRTY true
@@ -17,16 +17,16 @@
 #define GIT_AUTHOR_EMAIL L"1055907692@qq.com"
 
 // When HEAD was committed.
-#define GIT_COMMIT_DATE_ISO8601 L"2020-12-23 10:17:30 +0800"
+#define GIT_COMMIT_DATE_ISO8601 L"2020-12-23 10:51:02 +0800"
 
 // The subject line for the HEAD commit.
-#define GIT_COMMIT_SUBJECT L"set current path  ?"
+#define GIT_COMMIT_SUBJECT L"rename git project name"
 
 // The notes attached to the HEAD commit.
 #define GIT_COMMIT_BODY L""
 
 // The output from git --describe (e.g. the most recent tag)
-#define GIT_DESCRIBE L"4f43eb1"
+#define GIT_DESCRIBE L"cf1abd6"
 
 #define GIT_TAG L"unknown"
 #define GIT_VERSION L"?.?.?"
