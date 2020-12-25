eebb30247546d6fda5d87861a5fcdfd0a74f3322
daimingze <anyingshashou@qq.com>
2020-12-26 01:45:54 +0800

    programtime no impl

--- include/Base/ProgramTime.h
+++ include/Base/ProgramTime.h
@@ -6,12 +6,38 @@
 
 namespace gdf
 {
-
+template <typename TimeType, typename Rep, typename Period>
 struct GDF_EXPORT ProgramTime {
-    using duration = std::chrono::nanoseconds;
-    using rep = duration::rep;
-    using period = duration::period;
+    using rep = Rep;
+    using period = Period;
+    using duration = std::chrono::duration<rep, period>;
     using time_point = std::chrono::time_point<ProgramTime, duration>;
+
+    void Update() noexcept
+    {
+    }
+
+    void Reset() noexcept
+    {
+    }
+
+    Rep Elapsed() noexcept
+    {
+    }
+
+    Rep CurrentTime() const noexcept
+    {
+    }
+
+    typename TimeType::time_point LastTimePoint() const noexcept
+    {
+    }
+
+    typename TimeType::time_point CurrentTimePoint()const noexcept
+    {
+        
+    }
+
     static const bool is_steady = true;
     static auto now() noexcept -> time_point;
     static std::chrono::steady_clock::time_point programStartTime;
