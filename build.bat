8efdac144788c34466023cd75131fc7a6805a96d
HuiSeLiMing <1055907692@qq.com>
2021-01-20 11:24:04 +0800

    ms Clang

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
 @echo off
 cd /d %~dp0
 echo off
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 rem Generate Vsual Studio 2019 project.
-cmake -G "Ninja" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B out/build
+cmake -G"Visual Studio 16 2019" -T"ClangCl" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -S . -B out/build
 cd out/build
-ninja
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+MSBuild.exe ALL_BUILD.vcxproj -property:Configuration=Debug
